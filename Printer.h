b1c585f86ceabf5552362a0527bb8673f67a310f
TheGreatKingDK <dkim1000@msn.com>
2014-11-24 15:37:10 -0500

    init

---
+++ Printer.h
@@ -0,0 +1,32 @@
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+Monitor Printer {
+	unsigned int numStudents;
+	unsigned int numVendingMachines;
+	unsigned int numCouriers;
+	void flush();
+	string convert(int i);
+	string convert(char c);
+
+	string parent;
+	string watcard;
+	string nameServer;
+	string truck;
+	string plant;
+	string * studentBuffer;
+	string * vendingMachineBuffer;
+	string * courierBuffer;
+public:
+	enum Kind { Parent, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
+	Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
+	~Printer();
+	void print( Kind kind, char state );
+	void print( Kind kind, char state, int value1 );
+	void print( Kind kind, char state, int value1, int value2 );
+	void print( Kind kind, unsigned int lid, char state );
+	void print( Kind kind, unsigned int lid, char state, int value1 );
+	void print( Kind kind, unsigned int lid, char state, int value1, int value2 );
+};
