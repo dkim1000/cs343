b637982637b0f034064005c513ffa9badfc78b9c
TheGreatKingDK <dkim1000@msn.com>
2014-11-25 23:11:42 -0500

    Fixed Bank.h

--- Bank.h
+++ Bank.h
@@ -3,6 +3,7 @@ _Monitor Bank {
 	int * balance;
 	uCondition * aCond;
 public:
+	~Bank();
 	Bank( unsigned int numStudents );
 	void deposit( unsigned int id, unsigned int amount );
 	void withdraw( unsigned int id, unsigned int amount );
--- bank.h
+++
@@ -1,9 +0,0 @@
-_Monitor Bank {
-	unsigned int numStudents;
-	int * balance;
-	uCondition * aCond;
-public:
-	Bank( unsigned int numStudents );
-	void deposit( unsigned int id, unsigned int amount );
-	void withdraw( unsigned int id, unsigned int amount );
-};
